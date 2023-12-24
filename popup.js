388a0efb2cc31ab1cf3c886cbf1385de46a25db9
BB <buithienbaoo2@gmail.com>
2023-12-24 16:16:48 +0700

    Storage

--- popup.js
+++ popup.js
@@ -1,44 +1,49 @@
 /** @format */
 
-let infoDisplay = document.getElementById("info");
+document.addEventListener("DOMContentLoaded", function () {
+	// Load saved values from storage and update UI
+	chrome.storage.sync.get(
+		[
+			"defaultContextMode",
+			"defaultLanguage",
+			"backgroundColor",
+			"hideInputButtons",
+		],
+		function (result) {
+			document.getElementById("context_select").value =
+				result.defaultContextMode || "";
+			document.getElementById("language_select").value =
+				result.defaultLanguage || "";
+			document.getElementById("background_select").value =
+				result.backgroundColor || "";
+			document.getElementById("input_switch").checked =
+				result.hideInputButtons || false;
+		}
+	);
 
-const updatePopup = (result) => {
-	if (!chrome.runtime.lastError) {
-		infoDisplay.innerHTML = result;
-	}
-};
+	// Save values to storage when the Save button is clicked
+	document
+		.getElementById("saveButton")
+		.addEventListener("click", function () {
+			let newContextMode =
+				document.getElementById("context_select").value;
+			let newLanguage = document.getElementById("language_select").value;
+			let newBackgroundColor =
+				document.getElementById("background_select").value;
+			let hideInputButtons =
+				document.getElementById("input_switch").checked;
 
-document.addEventListener("DOMContentLoaded", () => {
-	console.log("DOM is loaded!");
-	let getBtn = document.getElementById("btn");
-	getBtn.addEventListener("click", () => {
-		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-			chrome.tabs.sendMessage(
-				tabs[0].id,
-				{ from: "popup", subject: "get_input" },
-				updatePopup
+			// Save values to storage
+			chrome.storage.sync.set(
+				{
+					defaultContextMode: newContextMode,
+					defaultLanguage: newLanguage,
+					backgroundColor: newBackgroundColor,
+					hideInputButtons: hideInputButtons,
+				},
+				function () {
+					console.log("Settings saved");
+				}
 			);
-			chrome.tabs.sendMessage(tabs[0].id, {
-				from: "extension",
-				subject: "transform_input",
-			});
 		});
-		let extensionIcon = document.createElement("span");
-		extensionIcon.innerHTML =
-			'<img src="path/to/your/icon.png" alt="Extension Icon" id="extension-icon">';
-		document.body.appendChild(extensionIcon);
-
-		// Bắt sự kiện click trên icon
-		let iconElement = document.getElementById("extension-icon");
-		iconElement.addEventListener("click", () => {
-			// Thực hiện các hành động khi icon được nhấp
-			// Ví dụ: Gửi tin nhắn tới background script hoặc content script
-			chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-				chrome.tabs.sendMessage(tabs[0].id, {
-					from: "popup",
-					subject: "icon_click",
-				});
-			});
-		});
-	});
 });
