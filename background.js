9cfd568889a768769a79bdf07be6cefbbe866510
Bang <122675456+ginganotnigg@users.noreply.github.com>
2024-01-02 22:46:41 +0700

    Add feature: Settings - set background color

--- background.js
+++ background.js
@@ -2,13 +2,39 @@
 * TEXT SELECTIONS
 */
 
-/*try {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { files: ['content.js'] }
-    )});
-}
-catch (e) {
-    console.log(e);
-} */
+
+chrome.contextMenus.create({
+    id: "revise",
+    title: "Revise",
+    contexts: ["selection"], 
+});
+chrome.contextMenus.create({
+    id: "translate",
+    title: "Translate",
+    contexts: ["selection"], 
+});
+chrome.contextMenus.create({
+    id: "send_to_chat",
+    title: "Send to AI-chat",
+    contexts: ["selection"], 
+});
+chrome.contextMenus.create({
+    id: "summarize",
+    title: "Summarize",
+    contexts: ["selection"], 
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === "revise") {
+        console.log(info.selectionText.toUpperCase());
+    }
+    else if (info.menuItemId === "translate") {
+        console.log(info.selectionText);
+    }
+    else if (info.menuItemId === "send_to_chat") {
+        console.log(info.selectionText);
+    }
+    else if (info.menuItemId === "summarize") {
+        console.log(info.selectionText);
+    }
+}); 
--- popup.js
+++ popup.js
@@ -1,16 +1,95 @@
-let infoDisplay = document.getElementById("info");
+const context = document.getElementById("context_select");
+const language = document.getElementById("language_select");
+const background = document.getElementById("background_select");
+const inputActions = document.getElementById("input_switch");
+const apiKey = document.getElementById("api_key_input");
 
-const updatePopup = (result) => {
-    if (!chrome.runtime.lastError) {
-        infoDisplay.innerHTML = result;
+const setBackground = (val) => {
+    let bgColor;
+    let textColor;
+    if (val === "blue") {
+        bgColor = 'lightblue';
+        textColor = 'midnightblue';
     }
+    else if (val === "green") {
+        bgColor = 'lightgreen';
+        textColor = 'darkgreen';
+    }
+    else if (val === "yellow") {
+        bgColor = 'bisque';
+        textColor = 'darkgoldenrod';
+    }
+    else {
+        bgColor = 'lightcoral';
+        textColor = 'maroon';
+    }
+    document.documentElement.style.setProperty('--main-bg-color', bgColor);
+    document.documentElement.style.setProperty('--main-text-color', textColor);
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {msg: "change_color", bgr: bgColor, text: textColor});
+    });
+};
+
+const updateSettings = (isSaved) => {
+    // API key
+    if (isSaved && apiKey.value == "") {
+        alert("Invalid API key: ChatGPT couldn't verify your API key");
+        return;
+    }
+    // Hide input actions
+
+    // Set background
+    setBackground(document.getElementById("background_select").value);
+    // Default language
+    
+    // Default context mode
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM is loaded!");
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { files: ['content.js'] }
-    )});
+    // Load saved values from storage and update UI
+    chrome.storage.sync.get(
+		[
+			"defaultContextMode",
+			"defaultLanguage",
+			"backgroundColor",
+			"hideInputButtons",
+            "apiKey",
+		],
+		function (result) {
+			context.value = result.defaultContextMode || "";
+			language.value = result.defaultLanguage || "";
+			background.value = result.backgroundColor || "";
+			inputActions.checked = result.hideInputButtons || false;
+            apiKey.value = result.apiKey || "";
+            updateSettings(false);
+        }
+	);
+
+	// Save values to storage when the Save button is clicked
+	document.getElementById("save_button")
+		.addEventListener("click", function () {
+			let newContextMode = context.value;
+			let newLanguage = language.value;
+			let newBackgroundColor = background.value;
+			let hideInputButtons = inputActions.checked;
+            let apiKeyText = apiKey.value;
+
+			// Save values to storage
+			chrome.storage.sync.set(
+				{
+					defaultContextMode: newContextMode,
+					defaultLanguage: newLanguage,
+					backgroundColor: newBackgroundColor,
+					hideInputButtons: hideInputButtons,
+                    apiKey: apiKeyText, 
+				},
+				function () {
+				}
+			);
+           
+            updateSettings(true);
+        });
+    
+    
 });
